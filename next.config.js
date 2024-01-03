1a4e4f80c0340bcc53b68779214813efe5de48ba
Rajat_Namdev <rajat.namdev@foregahead.io>
2024-01-03 13:49:54 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        topLevelAwait: true,
         appDir: true,
+        asyncWebAssembly: true,
+        buildHttp: true,
+        layers: true,
+        lazyCompilation: true,
+        outputModule: true,
+        syncWebAssembly: true,
+        topLevelAwait: true,
+      },
+      webpack: function (config, options) {
+        console.log(options.webpack.version); // Should be webpack v5 now
+        config.experiments = {
+            appDir: true,
+            topLevelAwait: true,
+        };
+        return config;
       },
 }
 
