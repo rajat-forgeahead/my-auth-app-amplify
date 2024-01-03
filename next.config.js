5a8964784a99c073f1677a4546557371452a3541
Rajat_Namdev <rajat.namdev@foregahead.io>
2024-01-03 15:09:28 +0530

    added dependencies

--- next.config.js
+++ next.config.js
@@ -12,7 +12,6 @@ const nextConfig = {
               options.defaultLoaders.babel,
               {
                 loader: '@mdx-js/loader',
-                options: pluginOptions.options,
               },
             ],})
         return config
